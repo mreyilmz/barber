57d44ae9b7406ad1ab49dbb136fb76c706ea2743
mreyilmz <mre.ylmaz@gmail.com>
2024-01-09 23:47:24 +0300

    first commit

---
+++ src/app/admin/components/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { User } from '../../../models/user';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent implements OnInit {
+  users: User[] = [];
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.getList();
+  }
+
+  getList() {
+    this.userService.getAll().subscribe((result) => {
+      this.users = result.data;
+      console.log(this.users);
+    });
+  }
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,28 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  authInterceptor,
+  loaderInterceptor,
+  notificationInterceptor,
+} from './admin/custom-functions/interceptors';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    provideToastr(),
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor,
+        loaderInterceptor,
+        notificationInterceptor,
+      ])
+    ),
+  ],
+};
